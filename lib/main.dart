5876f6d6f334f988dcc2aebe5d861ed4e35a7ee3
azkaBashir45 <azka.ngxoft@gmail.com>
2021-08-31 16:54:33 -0700

    card

--- lib/main.dart
+++ lib/main.dart
@@ -12,35 +12,82 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
-        backgroundColor: Colors.tealAccent,
-        body: SafeArea(
+          backgroundColor: Colors.tealAccent,
+          body: SafeArea(
             child: Column(
-          children: <Widget>[
-            // CircleAvatar(
-            //   backgroundImage: AssetImage('lib/images/flower.jpeg'),
-            //   radius: 70,
-            // ),
-            Text(
-              'Azka Bashir',
-              style: TextStyle(
-                color: Colors.black,
-                fontFamily: 'Pacifico',
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-              ),
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('lib/images/flower.jpeg'),
+                    backgroundColor: Colors.white,
+                    radius: 70,
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    'Azka Bashir',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Pacifico',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    'Flutter Instructor',
+                    style: TextStyle(
+                      color: Colors.black,
+                      letterSpacing: 2,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size: 30.0,
+                      color: Colors.tealAccent,
+                    ),
+                    title: Text(
+                      '+9234567890',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 30.0,
+                      color: Colors.tealAccent,
+                    ),
+                    title: Text(
+                      'azkabashir56@gmail.com',
+                      style: TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              'Flutter Instructor',
-              style: TextStyle(
-                color: Colors.black,
-                letterSpacing: 2,
-                fontWeight: FontWeight.bold,
-                fontSize: 14,
-              ),
-            ),
-          ],
-        )),
-      ),
+          )),
     );
   }
 }
